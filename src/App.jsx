fb899f37e5e1bd7a9045b3639c5b2395cdae7d26
João Vitor Montanari <joao.unipbr@gmail.com>
2022-10-15 12:16:13 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  const name = 'João'
+  const NameUpper = name.toUpperCase()
+  const url = 'https://via.placeholder.com/150'
+
+  function soma(a, b){
+    return(
+      a + b
+    );
+  }
+
+  return (
+    <div className='App'>
+      <h1>Hello World</h1>
+      <h3>Ola {name}</h3>
+      <p>
+        Soma de dois números: {soma(2,8)}
+      </p>
+      <h3>Nome maiusculo: {NameUpper}</h3>
+      <h2>IMAGEM:</h2>
+      <img src={url} alt="Imagem teste" />
+    </div>
+  );
+}
+
+export default App;
